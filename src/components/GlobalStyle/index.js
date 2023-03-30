a61755141c86927b4f2ccbd389fcccfe4e7ba8ef
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2023-03-30 11:05:02 +0700

    tes

---
+++ src/components/GlobalStyle/index.js
@@ -0,0 +1,5 @@
+import '../GlobalStyle/GlobalStyle.scss'
+function GlobalStyle({ children }){
+    return children;
+}
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "../src/components/GlobalStyle/index";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <App className={'test'} />
+    </GlobalStyle>
   </React.StrictMode>
 );
 
