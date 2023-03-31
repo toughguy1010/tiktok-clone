3c21d03f6b5c9d51c2dacc9df509a6100ae6cbee
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2023-03-31 10:14:31 +0700

    setup routes and layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/Layout/";
+import { Fragment } from "react";
 function App() {
   return (
-    <div className="App">
-        <h1>React</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            // const Layout = route.layout ? Fragment : DefaultLayout;
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if(route.layout === null) {
+              Layout = Fragment;
+            }
+            
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
